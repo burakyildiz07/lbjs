630847c89fd3f853f1f44ab96f64c5feb71befa4
Adem Kahraman <ademkahramanbote@gmail.com>
2017-07-21 12:45:41 +0300

    Bootstrap testi için layout ve deneme viewları oluşturuldu

---
+++ app/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>LARAVEL+BOOTSTRAP+JS</title>
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"><!--Bootstrap css dosyası-->
+</head>
+<body>
+    <h3>LARAVEL+BOOTSTRAP+JS Combo</h3>
+    @yield('content')
+</body>
+</html>
